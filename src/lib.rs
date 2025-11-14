662b1da30176b34738a0597c1d8bd159b2e07fca
Bart Massey <bart.massey@gmail.com>
2025-11-13 16:16:06 -0800

    cleaned up scoping, added tokio qualifiers

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,23 @@
-#[macro_export]
-macro_rules! _async_try {
-    ($stuff:tt) => {
-        async {
-            let mut wrapper = async || -> tokio::io::Result<_> {
-                Ok($stuff)
-            };
-            wrapper().await
-        }.await
-    };
+/*! Macros to use instead of `write!` and `writeln` in Tokio.
+
+Thanks to
+<https://users.rust-lang.org/t/equivalent-of-writeln-for-tokio/69002/5>
+for the starting idea.
+*/
+
+pub(crate) mod _async_try {
+    #[doc(hidden)]
+    #[allow(unused)]
+    macro_rules! async_try {
+        ($stuff:tt) => {
+            async {
+                let mut wrapper = async || -> tokio::io::Result<_> {
+                    Ok($stuff)
+                };
+                wrapper().await
+            }.await
+        };
+    }
 }
 
 /// Equivalent to `writeln!()` but with `tokio` async I/O.
@@ -18,10 +28,10 @@ macro_rules! awrite {
     ($dst:expr, $fmt:literal, $($arg:expr),*) => {{
         let mut buf: Vec<u8> = Vec::new();
         std::write!(buf, $fmt, $($arg),*).unwrap();
-        $dst.write_all(&buf).await
+        tokio::io::AsyncWriteExt::write_all($dst, &buf).await
     }};
     ($dst:expr, $fmt:literal) => {
-        $dst.write_all($fmt.as_bytes()).await
+        tokio::io::AsyncWriteExt::write_all($dst, $fmt.as_bytes()).await
     };
 }
 
@@ -31,11 +41,11 @@ macro_rules! awrite {
 /// Returns `tokio::io::Result<()>`.
 #[macro_export]
 macro_rules! awriteln {
-    ($dst:expr, $fmt:literal, $($arg:expr),*) => {awrite::_async_try!{{
+    ($dst:expr, $fmt:literal, $($arg:expr),*) => {$crate::async_try!{{
         awrite!($dst, $fmt, $($arg),*)?;
         awrite!($dst, "\r\n")?;
     }}};
-    ($dst:expr, $fmt:literal) => {awrite::_async_try!{{
+    ($dst:expr, $fmt:literal) => {$crate::async_try!{{
         awrite!($dst, $fmt)?;
         awrite!($dst, "\r\n")?;
     }}};
